094f3e09e40790ae71fa6b490872064225725d4a
poberherr <patrick.oberherr@gmail.com>
2015-01-09 20:02:32 +0100

    First Initial connection with hardcoded user

---
+++ database-setup.js
@@ -0,0 +1,18 @@
+var pg = require('pg');
+var conString = "postgres://patrick:@localhost/cratings";
+
+pg.connect(conString, function(err, client, done) {
+    if(err) {
+        return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+        //call `done()` to release the client back to the pool
+        done();
+
+        if(err) {
+            return console.error('error running query', err);
+        }
+        console.log(result.rows[0].number);
+        //output: 1
+    });
+});
