7cc7458e9442722b37ce949593af91df47b09599
poberherr <patrick.oberherr@gmail.com>
2015-01-10 00:39:11 +0100

    Add more routes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var Hapi = require('hapi');
 
+
 var Review = require('./lib/models/review');
+var Reviews = require('./lib/collections/reviews');
+
+var reviews = new Reviews();
+
+
+
 
 var serverConfig = {
     host: '0.0.0.0',
@@ -16,9 +23,28 @@ server.route({
     method: 'POST',
     path:'/reviews',
     handler: function (request, reply) {
-        console.log(request)
-        var review = new Review(request.payload)
-        reply(review.toJSON()).code(201);
+        var review = new Review(request.payload);
+        reviews.add(review);
+        var response = review.toJSON();
+        reply(response).code(201);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path:'/reviews',
+    handler: function (request, reply) {
+        var response = reviews.toJSON();
+        reply(response).code(200);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path:'/reviews/{id}',
+    handler: function (request, reply) {
+        var response = reviews.get(request.params.id).toJSON();
+        reply(response).code(200);
     }
 });
 
